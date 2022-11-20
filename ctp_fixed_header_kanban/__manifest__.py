17cc51b7546183b1513804fa9a490cf174cf42ae
BOMEN <bomen21x@gmail.com>
2022-11-21 01:21:36 +0700

    Update __manifest__.py

--- ctp_fixed_header_kanban/__manifest__.py
+++ ctp_fixed_header_kanban/__manifest__.py
@@ -2,33 +2,36 @@
 ###################################################################################
 #
 #    Cybernetics Plus Co., Ltd.
-#    Cybernetics Plus Co., Ltd. Sticky Header Kanban Displaying View within Odoo.
+#    Sticky Header Kanban Displaying View within Odoo.
 #
 ###################################################################################
 
 {
-    'name': 'Sticky Header in Kanban',
-    'version': '15.0.0.0.1',
-    'summary': """ 
-            Cybernetics Plus Co., Ltd. Sticky Header Kanban Displaying View within Odoo.
+    "name": "Sticky Header in Kanban",
+    "version": "15.0.0.0.1",
+    "summary": """ 
+            Sticky Header Kanban Displaying View within Odoo.
             """,
-    'description': """ 
-            Cybernetics Plus Co., Ltd. Sticky Header Kanban Displaying View within Odoo.
+    "description": """ 
+            Sticky Header Kanban Displaying View within Odoo.
             """,
-    'author': 'Cybernetics Plus Co., Ltd.',
-    'website': 'https://www.cybernetics.plus',
-    'live_test_url': 'https://www.cybernetics.plus',
-    'images': ['static/description/banner.png'],
-    'license': 'LGPL-3',
-    'installable': True,
-    'application': True,
-    'auto_install': False,
-    'contributors': [
-        'Developer <dev@cybernetics.plus>',
+    "author": "Cybernetics Plus",
+    "company": "Cybernetics Plus Co., Ltd.",
+    "maintainer": "Cybernetics Plus Co., Ltd.",
+    "website": "https://www.cybernetics.plus",
+    "live_test_url": "https://www.cybernetics.plus",
+    "images": ["static/description/banner.png"],
+    "category": "Tools",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "contributors": [
+        "C+ Developer <dev@cybernetics.plus>",
     ],
-    'assets': {
+    "assets": {
         "web.assets_backend": [
-            'ctp_fixed_header_kanban/static/src/scss/kanban.scss',
+            "ctp_fixed_header_kanban/static/src/scss/kanban.scss",
         ],
     },
 }
